8b46cf5606a9a10ac463be55b4b89f57074619de
lilychan16 <chen460@indiana.edu>
2024-04-04 23:34:07 -0400

    complete a5 server

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import cors from "cors";
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from './Kanbas/Modules/routes.js';
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 
 const app = express();
 app.use(cors());
 // make sure this statement occurs AFTER setting up CORS
 app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
 Lab5(app)
 Hello(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000);
