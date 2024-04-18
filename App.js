481d5008c0d6eba60b2021817cc055950c0abbb5
maggieliu <magliudh@gmail.com>
2024-04-17 21:59:47 -0400

    commit before pulling

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-
 const CONNECTION_STRING =
   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 const DB_NAME = process.env.DB_NAME;
