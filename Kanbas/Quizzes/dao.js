6b29bb5a594b39c8bbd06256eb2ad23f76f85af9
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-04-18 21:03:20 -0400

    pull server-side from quiz-list

---
+++ Kanbas/Quizzes/dao.js
@@ -0,0 +1,15 @@
+import model from "./model.js";
+
+export const createQuiz = (quiz) => {
+  delete quiz._id;
+  return model.create(quiz);
+};
+
+export const findAllQuizzes = () => model.find();
+
+export const findQuizzesByCourse = (course) => model.find({ course: course });
+
+export const deleteQuiz = (quizId) => model.deleteOne({ _id: quizId });
+
+export const updateQuiz = (quizId, quiz) =>
+  model.updateOne({ _id: quizId }, { $set: quiz });
