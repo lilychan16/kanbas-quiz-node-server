f8d9eb49e2a49c9808be38525dd25dc8fa0a3a1e
BINGCHUN CHANG <122491347+CharlieCBC@users.noreply.github.com>
2024-04-18 21:09:11 -0400

    new changes from quiz-list

--- App.js
+++ App.js
@@ -9,10 +9,12 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import QuizRoutes from "./Kanbas/Quizzes/routes.js";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas_quiz';
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas_quiz";
 const DB_NAME = process.env.DB_NAME;
-mongoose.connect(CONNECTION_STRING, {dbName: DB_NAME});
+mongoose.connect(CONNECTION_STRING, { dbName: DB_NAME });
 
 const app = express();
 app.use(
@@ -21,7 +23,7 @@ app.use(
     credentials: true,
     // restrict cross origin resource sharing to the react application
     origin: process.env.FRONTEND_URL,
-  })
+  }),
 );
 // make sure this statement occurs AFTER setting up CORS
 app.use(express.json());
@@ -29,14 +31,14 @@ app.use(express.json());
 const sessionOptions = {
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: "https://kanbas-node-server-app-a6-b718.onrender.com"
+    domain: "https://kanbas-node-server-app-a6-b718.onrender.com",
   };
 }
 app.use(session(sessionOptions));
@@ -44,6 +46,7 @@ app.use(session(sessionOptions));
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+QuizRoutes(app);
 AssignmentRoutes(app);
 Lab5(app);
 Hello(app);
